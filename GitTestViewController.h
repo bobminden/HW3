8f298da1c9e96707c04a52ccc03145eb5deaefd8
Robert Walker <bobfwalker@gmail.com>
2013-04-25 10:15:15 -0400

    Storyboard changes and added a view controller class

---
+++ GitTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  GitTestViewController.h
+//  Games
+//
+//  Created by Robert Walker on 4/25/13.
+//  Copyright (c) 2013 Robert Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GitTestViewController : UIViewController
+
+@end
---
+++ GitTestViewController.m
@@ -0,0 +1,34 @@
+//
+//  GitTestViewController.m
+//  Games
+//
+//  Created by Robert Walker on 4/25/13.
+//  Copyright (c) 2013 Robert Walker. All rights reserved.
+//
+
+#import "GitTestViewController.h"
+
+@interface GitTestViewController ()
+
+@end
+
+@implementation GitTestViewController
+
+- (IBAction)message:(id)sender
+{
+    NSLog(@"This worked.");
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
